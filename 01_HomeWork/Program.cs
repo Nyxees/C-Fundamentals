75ec1f3d694807a090e8338e896362e19c8c7e60
Саша Гангур <deathtapochekk@gmail.com>
2023-04-27 11:22:08 +0300

    Add project files.

---
+++ 01_HomeWork/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 1 Task
+        int a;
+        Console.Write("Enter the side length of the square: ");
+        a = int.Parse(Console.ReadLine());
+
+        int area = a * a;
+        int perimeter = 4 * a;
+
+        Console.WriteLine("The area of the square is " + area);
+        Console.WriteLine("The perimeter of the square is " + perimeter);
+
+        // 2 Task
+        string name;
+        int age;
+
+        Console.Write(" What is your name? ");
+        name = Console.ReadLine();
+
+        Console.Write($"How old are you, {name}? ");
+        age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Your name is " + name + " and you are " + age + " years old.");
+
+        // 3 Task
+        double r;
+
+        Console.Write("Enter the radius of the circle: ");
+        r = double.Parse(Console.ReadLine());
+
+        double pi = Math.PI;
+        double length = 2 * pi * r;
+        double CircleArea = pi * r * r;
+        double volume = 4.0 / 3.0 * pi * r * r * r;
+
+        Console.WriteLine("The length of the circle is " + length);
+        Console.WriteLine("The area of the circle is " + CircleArea);
+        Console.WriteLine("The volume of the circle is " + volume);
+    }
+}
---
+++ 02_HomeWork/Program.cs
@@ -0,0 +1,90 @@
+﻿enum HTTPError
+{
+    BadRequest = 400,
+    Unauthorized = 401,
+    PaymentRequired = 402
+}
+
+struct Dog
+{
+    public string name;
+    public string mark;
+    public int age;
+
+    public override string ToString()
+    {
+        return "Name: " + name + ", Mark: " + mark + ", Age: " + age;
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 1 Task
+        
+        Console.Write("Enter the first number: ");
+        float num1 = float.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        float num2 = float.Parse(Console.ReadLine());
+
+        Console.Write("Enter the third number: ");
+        float num3 = float.Parse(Console.ReadLine());
+
+        bool allInRange = num1 >= -5.0 && num1 <= 5.0 && num2 >= -5.0 && num2 <= 5.0 && num3 >= -5.0 && num3 <= 5.0;
+
+        if (allInRange)
+        {
+            Console.WriteLine("All numbers are in the range [-5, 5].");
+        }
+        else
+        {
+            Console.WriteLine("Not all numbers are in the range [-5, 5].");
+        }
+
+        // 2 Task
+
+        Console.Write("Enter the first number: ");
+        int Num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int Num2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the third number: ");
+        int Num3 = int.Parse(Console.ReadLine());
+
+        int max = Math.Max(Num1, Math.Max(Num2, Num3));
+        int min = Math.Min(Num1, Math.Min(Num2, Num3));
+
+        Console.WriteLine("The maximum number is: " + max);
+        Console.WriteLine("The minimum number is: " + min);
+
+        // 3 Task
+
+        Console.Write("Enter the number of an HTTP error (400, 401, or 402): ");
+        int errorCode = int.Parse(Console.ReadLine());
+
+        if (Enum.IsDefined(typeof(HTTPError), errorCode))
+        {
+            HTTPError error = (HTTPError)errorCode;
+            Console.WriteLine("The error name is: " + error.ToString());
+        }
+        else
+        {
+            Console.WriteLine("Invalid error code entered.");
+        }
+
+        // 4 Task
+
+        Dog myDog;
+        Console.Write("Enter the dog's name: ");
+        myDog.name = Console.ReadLine();
+        Console.Write("Enter the dog's mark: ");
+        myDog.mark = Console.ReadLine();
+        Console.Write("Enter the dog's age: ");
+        myDog.age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(myDog.ToString());
+
+    }
+}
